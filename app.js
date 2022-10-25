e953031729f24e19919a53372b141f236aef443a
JadenTG <111470273+JadenTG@users.noreply.github.com>
2022-10-25 16:10:57 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+/* 
+//              Random Number Generator                 //
+const randomNumber = document.getElementById("display");
+const btt = document.getElementById("btn");
+//      What ties the Button to the function       //
+btt.addEventListener('click', generatenumber);
+
+function generatenumber()
+{
+    //          Generates the Number            //
+    const randomnumber = Math.random(Math.random() * 10 + 1);
+    console.log(randomnumber);
+    document.getElementById("display").textContent = randomnumber;
+}
+*/
+
+//              Random Color Generator                //
+const display = document.getElementById("display"); 
+const button = document.getElementById("btn");
+
+//         What ties the Button to the function     //
+button.addEventListener("click", GenerateColor)
+
+function GenerateColor()
+{
+    //      Connects the Colors Together       //
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    //    This is String for the Color Generator   //
+    let color = "rgb(" + r + "," + g +"," + b + ")";
+
+    display.style.backgroundColor = color; 
+}
+
+setInterval(GenerateColor, 1000);
